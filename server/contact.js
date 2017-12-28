c23f08a4d2220b575949c7a36716bc3d30ab56f1
Ryan Geyer <gyanreyer@gmail.com>
2017-12-28 00:43:29 -0500

    Added in all assets, set up basic contact backend TODO: -Finish front-end for contact form -Figure out if contact form actually works

--- server/contact.js
+++ server/contact.js
@@ -1,9 +1,48 @@
-const mail = require('nodemailer');
+//const mail = require('nodemailer');
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});//N Virginia
 
 const sendContact = (req, res) => {
     //Verify body
+    const fromName = `${req.body.name}`;
+    const fromEmail = `${req.body.email}`;
+    const message = `${req.body.message}`;
 
+    const params = {
+        Destination: {
+            ToAddresses: [
+                'gyanreyer@gmail.com'
+            ]
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: 'UTF-8',
+                    Data: `<p>${message}</p><p>-<a href="mailto:${fromEmail}">${fromName}</a></p>`
+                },
+                Text: {
+                    Charset: 'UTF-8',
+                    Data: `${message} -${fromName} (${fromEmail})`
+                }
+            },
+            Subject: {
+                Charset: 'UTF-8',
+                Data: `Contact message from ${fromName}`
+            }
+        },
+        Source: 'contact@rgeyer.com',
+        ReplyToAddresses: [
+            fromEmail
+        ]
+    };
 
+    const sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+
+    sendPromise.then((data)=>{
+            console.log(data.MessageId);
+        }).catch((err)=>{
+            console.error(err, err.stack);
+        });
 };
 
 module.exports = sendContact;
--- server/router.js
+++ server/router.js
@@ -2,14 +2,24 @@ const projectPage = require('./projects');
 const contact = require('./contact');
 
 const router = (app) => {
-  app.post('/contact');
-  app.get('/project/:name', projectPage);
-  app.get('/', (req, res) => {
-    res.render('home');
-  });
-  app.get('/*', (req, res) => {
-    res.render('notFound');
-  });
+    app.get('/Painter', (req, res) => {
+        res.render('painter');
+    });
+    app.get('/Soundwaves', (req, res) => {
+        res.render('soundwaves');
+    });
+    app.get('/MapSeek', (req, res) => {
+        res.render('mapseek');
+    });
+
+    app.post('/contact', contact);
+    app.get('/project/:name', projectPage);
+    app.get('/', (req, res) => {
+      res.render('home');
+    });
+    app.get('/*', (req, res) => {
+      res.render('notFound');
+    });
 };
 
 module.exports = router;
