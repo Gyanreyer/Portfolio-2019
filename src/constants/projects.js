3b4240a61da865bd23d7746fba52a3a4807fd2f1
Ryan Geyer <gyanreyer@gmail.com>
2019-06-09 01:05:02 -0400

    Update image assets

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -8,7 +8,7 @@ export default [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     technologies: ["JavaScript", "React", "CSS", "Python", "Django"],
     primaryLink: "http://waymark.com",
-    image: require("../resources/thumbnails/waymark.jpg"),
+    image: require("../resources/thumbnails/waymark.png"),
     primaryColor: "#CCC",
     textTheme: "dark"
   },
